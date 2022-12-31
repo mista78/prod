289963ab4335176afd4c955f0a2d739d791ce149
Karim Maoulida <karimmaoulida@MacBook-Pro-de-Karim.local>
2022-12-31 01:12:20 +0100

    Remove unsable table

--- App/Dep/Database.php
+++ App/Dep/Database.php
@@ -6,17 +6,5 @@ return [
         'base' => denv('DEVLIE_BASE'),
         'user' => denv('DEVLIE_USER'),
         'pass' => denv('DEVLIE_PASS'),
-    ],
-    "wordpress" => [
-        'host' => denv('DEVLIE_HOST'),
-        'base' => denv('DEVLIE_ADMIN_DB'),
-        'user' => denv('DEVLIE_USER'),
-        'pass' => denv('DEVLIE_PASS'),
-    ],
-    "prod" => [
-        'host' => denv('DEVLIE_HOST'),
-        'base' => denv('DEVLIE_BASE'),
-        'user' => denv('DEVLIE_USER'),
-        'pass' => denv('DEVLIE_PASS'),
     ]
 ];
